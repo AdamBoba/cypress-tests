007c083de9b1e032940d6b3075cac8065d1805e8
AdamBoba <adamekboba@gmail.com>
2025-05-23 12:30:37 +0200

    Aktualizacja testów w pliku todo.cy.js

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,62 +1,67 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+// Testy dostosowane do strony https://aheaco.t24.ovh/
+// Używają aktualnych id przycisków: addCityBtn, generateBtn, calculateBtn, stopBtn, addCoordBtn
 
-describe('example to-do app', () => {
+describe('ACO TSP App - https://aheaco.t24.ovh/', () => {
   beforeEach(() => {
     cy.visit('https://aheaco.t24.ovh/');
   });
-  it('Sprawdzenie generatora miast', () => {
-    cy.get('#generateBtn').click(); // Kliknij przycisk "Wygeneruj miasta"
-    cy.get('.city').should('have.length.at.least', 1); // Sprawdź, czy miasta zostały wygenerowane
-  });
-  it('Sprawdzenie elementów strony', () => {
-    cy.get('header').should('be.visible'); // Sprawdź, czy nagłówek jest widoczny
-    cy.get('button').should('have.length.at.least', 1); // Sprawdź, czy są przyciski
-    cy.get('#map').should('exist'); // Sprawdź, czy mapa istnieje
+
+  it('Powinien wygenerować miasta po kliknięciu "Wygeneruj miasta"', () => {
+    cy.get('#generateBtn').click();
+    cy.get('.city').should('have.length.at.least', 1);
   });
 
-  it('Sprawdzenie generatora miast', () => {
-    cy.get('#generate-cities').click(); // Kliknij przycisk generatora
-    cy.get('.city').should('have.length.at.least', 1); // Sprawdź, czy miasta zostały wygenerowane
+  it('Powinien dodać miasto po kliknięciu "Dodaj"', () => {
+    // Wprowadź przykładowe współrzędne X i Y, jeśli są wymagane
+    cy.get('input[placeholder="X"]').type('100');
+    cy.get('input[placeholder="Y"]').type('200');
+    cy.get('#addCoordBtn').click();
+    cy.get('.city').should('have.length.at.least', 1);
   });
 
-  it('Sprawdzenie dodawania miast przez kliknięcie', () => {
-    cy.get('#add-city').click(); // Kliknij przycisk dodawania miasta
-    cy.get('.city').should('have.length.at.least', 1); // Sprawdź, czy miasto zostało dodane
+  it('Powinien umożliwić dodawanie miast klikając "Dodaj klikając"', () => {
+    cy.get('#addCityBtn').click();
+    // Załóżmy, że po aktywacji trybu można kliknąć na mapie (np. #map)
+    cy.get('#map').click(100, 100);
+    cy.get('.city').should('have.length.at.least', 1);
   });
 
-  it('Sprawdzenie usuwania miast', () => {
-    cy.get('#generate-cities').click(); // Wygeneruj miasta
-    cy.get('.city').first().find('.delete-button').click(); // Usuń pierwsze miasto
-    cy.get('.city').should('have.length.lessThan', 5); // Sprawdź, czy liczba miast się zmniejszyła
+  it('Powinien usuwać miasto (jeśli przycisk usuwania istnieje)', () => {
+    cy.get('#generateBtn').click();
+    // Zakładamy, że przycisk usuwania miasta ma klasę .delete-button
+    cy.get('.city').first().find('.delete-button').click({force: true});
+    cy.get('.city').should('have.length.lessThan', 10); // 10 to domyślna liczba miast
   });
 
-  it('Sprawdzenie obliczanie najkrótszej trasy', () => {
-    cy.get('#calculate-route').click(); // Kliknij przycisk obliczania trasy
-    cy.get('.route').should('exist'); // Sprawdź, czy trasa została obliczona
+  it('Powinien obliczać trasę po kliknięciu "Oblicz trasę"', () => {
+    cy.get('#calculateBtn').click();
+    cy.get('.route').should('exist');
   });
 
-  it('Sprawdzenie istnienia legendy', () => {
-    cy.get('#legend').should('be.visible'); // Sprawdź, czy legenda jest widoczna
+  it('Powinien mieć widoczną legendę, jeśli istnieje', () => {
+    cy.get('#legend').should('be.visible');
   });
 
-  it('Sprawdzenie istnienia 5 tras po obliczeniu tras', () => {
-    cy.get('#calculate-route').click(); // Kliknij przycisk obliczania tras
-    cy.get('.route').should('have.length', 5); // Sprawdź, czy wygenerowano 5 tras
+  it('Powinien wyświetlić statystyki po obliczeniu tras', () => {
+    cy.get('#calculateBtn').click();
+    cy.get('#statistics').should('be.visible');
   });
 
-  it('Sprawdzenie istnienia statystyk po obliczeniu tras', () => {
-    cy.get('#calculate-route').click(); // Kliknij przycisk obliczania tras
-    cy.get('#statistics').should('be.visible'); // Sprawdź, czy statystyki są widoczne
+  it('Powinien mieć widoczne przyciski operacji', () => {
+    cy.get('#generateBtn').should('be.visible');
+    cy.get('#addCityBtn').should('be.visible');
+    cy.get('#calculateBtn').should('be.visible');
+    cy.get('#stopBtn').should('exist');
+  });
+
+  it('Powinien mieć widoczny input do importu pliku', () => {
+    cy.contains('Wybierz plik...').should('be.visible');
+  });
+
+  it('Powinien mieć widoczne sekcje "Wizualizacja najkrótszej trasy" oraz "Histogram odległości tras"', () => {
+    cy.contains('Wizualizacja najkrótszej trasy').should('be.visible');
+    cy.contains('Histogram odległości tras').should('be.visible');
   });
 });
